598a4a9b922c725de9673d8c945476f0a7ffa28a
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-27 10:30:04 +0300

    CustomStack

---
+++ Class/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Stack<T>: IEnumerable<T>
+    {
+        public List<T> Elements { get; set; }
+
+        public Stack()
+        {
+            this.Elements = new List<T>();
+        }
+        public void Push(T element)
+        {
+            this.Elements.Add(element);
+        }
+
+        public void Pop()
+        {
+            if (this.Elements.Count==0)
+            {
+                throw new ArgumentException("No elements");
+            }
+            this.Elements.RemoveAt(this.Elements.Count-1);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Elements.Count-1; i > 0; i--)
+            {
+                yield return this.Elements[i];
+            }
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Class/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+   public class StartUp
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack <string>myStack = new Stack <string>();
+            while (input!="END")
+            {
+                string[] args = input.Split(new []{',',' '},StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    switch (args[0])
+                    {
+                        case "Push":
+                            for (int i = 1; i < args.Length; i++)
+                            {
+                                myStack.Push(args[i]);
+                            }
+                            break;
+                        case "Pop":
+                            myStack.Pop();
+                            break;
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                input = Console.ReadLine();
+            }
+            if (myStack.Elements.Count > 0)
+            {
+                for (int i = myStack.Elements.Count - 1; i >=0; i--)
+                {
+                    Console.WriteLine(myStack.Elements[i]);
+                }
+                for (int i = myStack.Elements.Count - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(myStack.Elements[i]);
+                }
+            }
+          
+           
+        }
+    }
+}
