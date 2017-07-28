fb1b286a3a065c8dce35237c6812b8f267f40dbe
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-28 13:59:39 +0300

    Froggy

---
+++ Froggy/Lake.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froggy
+{
+    public class Lake:IEnumerable<int>
+    {
+        public List<int> Stones { get; set; }
+
+        public Lake(List<int>stones)
+        {
+            this.Stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            
+            for (int i = 0; i <= this.Stones.Count-1; i+=2)
+            {
+                yield return this.Stones[i];
+            }
+            if (this.Stones.Count % 2 == 0)
+            {
+                for (int i = this.Stones.Count - 1; i >= 1; i -= 2)
+                {
+                    yield return this.Stones[i];
+                }
+            }
+            else
+            {
+                for (int i = this.Stones.Count-2; i >= 1; i-=2)
+                {
+                    yield return this.Stones[i];
+                }
+            }
+
+           
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Froggy/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froggy
+{
+   public class StartUp
+    {
+       public static void Main()
+       {
+            List<int> input = Console.ReadLine().Split(new[] {',', ' '}, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse).ToList();
+            Lake lake = new Lake(input);
+
+           Console.WriteLine(string.Join(", ",lake));
+
+       }
+    }
+}
