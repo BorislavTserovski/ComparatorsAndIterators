48b6a3197a199781ce0e9b385c5b879f8ee3b30c
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-08-01 10:12:17 +0300

    Equality

---
+++ EqualityLogic/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityLogic
+{
+   public class Person:IComparable<Person>
+    {
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.ToLower()==other.Name.ToLower()&&this.Age==other.Age)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person objAsPerson = obj as Person;
+            return this.Name.ToLower() == objAsPerson.Name.ToLower() && this.Age == objAsPerson.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+    }
+}
---
+++ EqualityLogic/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+       public static void Main()
+       {
+           int n = int.Parse(Console.ReadLine());
+            SortedSet<Person> peopleSorted = new SortedSet<Person>();
+            HashSet<Person> peopleHash = new HashSet<Person>();
+           for (int i = 0; i < n; i++)
+           {
+               string[] input = Console.ReadLine().Split();
+               string name = input[0];
+               int age = int.Parse(input[1]);
+               Person person = new Person(name, age);
+               peopleSorted.Add(person);
+               peopleHash.Add(person);
+           }
+           Console.WriteLine(peopleSorted.Count);
+           Console.WriteLine(peopleHash.Count);
+       }
+    }
+}
